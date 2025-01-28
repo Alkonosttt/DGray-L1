a7724c4d8e4f3d85454a9453e4fb2c8c7e3adee8
Alkonosttt <vakhlaieva.vysotska.a@gmail.com>
2025-01-28 21:46:59 +0100

    Added Post into Admin panel and post display

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+posts = Post.objects.all().order_by('-date')
 def posts_list(request):
-    return render(request, 'posts/posts_list.html')
+    return render(request, 'posts/posts_list.html', {'posts': posts})
